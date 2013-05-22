f6647d7fc0f0b14d143a369af5e2a7e1248f60c2
Jamie Ly <me@jamie.ly>
2013-05-21 21:14:25 -0400

    Implemented many of the utility functions. Tests to come.

--- src/airplane/g2/PlanePair.java
+++ src/airplane/g2/PlanePair.java
@@ -1,5 +1,39 @@
 package airplane.g2;
 
+import airplane.sim.Plane;
+
 public class PlanePair {
-	//TODO: 
+	private Plane first;
+	private Plane second;
+	private double distance;
+	
+	public PlanePair(Plane first, Plane second, double distance) {
+		setFirst(first);
+		setSecond(second);
+		setDistance(distance);
+	}
+
+	public Plane getFirst() {
+		return first;
+	}
+
+	public void setFirst(Plane first) {
+		this.first = first;
+	}
+
+	public Plane getSecond() {
+		return second;
+	}
+
+	public void setSecond(Plane second) {
+		this.second = second;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
 }
--- src/airplane/g2/util/PlaneUtil.java
+++ src/airplane/g2/util/PlaneUtil.java
@@ -6,21 +6,148 @@ import airplane.g2.PlanePair;
 import airplane.sim.Plane;
 
 public class PlaneUtil {
+	/**
+	 * Returns the distance of the plane from its current location to its destination.
+	 * 
+	 * @param plane
+	 * @return
+	 */
+	public static double distanceToDestination(Plane plane) {
+		return plane.getLocation().distance(plane.getDestination());
+	}
+	
+	/**
+	 * Returns the distance between two planes.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double distanceBetweenPlanes(Plane a, Plane b) {
+		return a.getLocation().distance(b.getLocation());
+	}
+	
+	/**
+	 * Returns the distance from the plane's current location to its destination
+	 * truncated to nearest integer.
+	 * 
+	 * @param plane
+	 * @return The steps to the destination
+	 */
 	public static int stepsToDestination(Plane plane) {
-		return (int)plane.getLocation().distance(plane.getDestination());
+		return (int) distanceToDestination(plane);
 	}
 	
+	/**
+	 * Returns the number of rounds until departure. 0 if the departure time has
+	 * already passed.
+	 * @param plane
+	 * @param round
+	 * @return
+	 */
+	public static int roundsUntilDeparture(Plane plane, int round) {
+		int delta = plane.getDepartureTime() - round;
+		return delta < 0 ? 0 : delta;
+	}
+	
+	/**
+	 * Returns the number of steps until departure, that is the number of rounds times
+	 * the number of steps per rounds.
+	 * @param plane
+	 * @param round
+	 * @return
+	 */
+	public static int stepsUntilDeparture(Plane plane, int round) {
+		return roundsUntilDeparture(plane, round) * stepsPerRound();
+	}
+	
+	/**
+	 * Returns the number of steps to the destination factoring in the steps that 
+	 * could've been taken if the plane had started moving immediately. 
+	 * 
+	 * @param plane
+	 * @param round
+	 * @return
+	 */
+	public static int stepsToDestinationIncludingDeparture(Plane plane, int round) {
+		return stepsToDestination(plane) +
+				stepsUntilDeparture(plane, round);
+	}
+	
+	/**
+	 * The number of steps that can be taken each round.
+	 * @return Returns the number of steps that can be taken each round. 
+	 */
+	public static int stepsPerRound() {
+		return 1;
+	}
+	
+	/**
+	 * Returns the plane that will land first given their current locations.
+	 * 
+	 * @param p1
+	 * @param p2
+	 * @return
+	 */
 	public static Plane getFirstToLand(Plane p1, Plane p2) {
-		return null;
+		return stepsToDestination(p1) > stepsToDestination(p2) ? p2 : p1;
+	}
+		
+	/**
+	 * Returns the plane that will land first given the current plane location 
+	 * and its departure time.
+	 * 
+	 * @param p1
+	 * @param p2
+	 * @return
+	 */
+	public static Plane getFirstToLandGivenDeparture(Plane p1, Plane p2, int round) {
+		return stepsToDestinationIncludingDeparture(p1, round) > 
+			stepsToDestinationIncludingDeparture(p2, round)? p2 : p1;
 	}
 	
 	
+	/**
+	 * Returns a two-dimensional array of distances.
+	 * 
+	 * @param planes
+	 * @return
+	 */
 	public static double[][] getDistances(ArrayList<Plane> planes) {
-		return null;
+		int count = planes.size();
+		double[][] distances = new double[count][count];
+		
+		for(int i = 0; i < count; i ++) {
+			for(int j = i; j < count; j++) {
+				distances[i][j] = (i == j) ? 
+						0 : distanceBetweenPlanes(planes.get(i), planes.get(j));
+				// mirrored
+				distances[j][i] = distances[i][j];
+			}
+		}
+		return distances;
 	}
 	
-	public static ArrayList<PlanePair> detectCollisions(ArrayList<Plane> planes, int radius) {
-		return null;
+	/**
+	 * Returns a list of pairs of colliding planes.
+	 * 
+	 * @param planes
+	 * @param distanceThreshold
+	 * @return
+	 */
+	public static ArrayList<PlanePair> detectCollisions(ArrayList<Plane> planes, int distanceThreshold) {
+		double[][] distances = getDistances(planes);
+		int count = planes.size();
+		ArrayList<PlanePair> collisionPairs = new ArrayList<PlanePair>();
+		for(int i = 0; i < count; i ++) {
+			for(int j = i; j < count; j++) {
+				double dist = distances[i][j];
+				if(dist > distanceThreshold) continue;
+				
+				PlanePair pair = new PlanePair(planes.get(i), planes.get(j), dist);
+				collisionPairs.add(pair);
+			}
+		}
+		return collisionPairs;
 	}
 	
 	
