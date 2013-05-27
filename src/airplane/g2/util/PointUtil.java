9dc803da394e7ab64bd920d3b183a4ff0d1cd86c
Jamie Ly <me@jamie.ly>
2013-05-26 21:49:35 -0400

    Added midpoint method to calculate midpoint between two points.

--- src/airplane/g2/util/PointUtil.java
+++ src/airplane/g2/util/PointUtil.java
@@ -1,5 +1,6 @@
 package airplane.g2.util;
 
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 public class PointUtil {
@@ -7,4 +8,9 @@ public class PointUtil {
 	public static Point2D.Double addPoints(Point2D.Double point1, Point2D.Double point2) {
 		return new Point2D.Double(point1.x + point2.x, point1.y + point2.y);
 	}
+	
+	public static Point2D.Double midpoint(Point2D.Double a, Point2D.Double b) {
+		return new Point2D.Double((a.x+b.x)/2.0, (a.y+b.y)/2.0);
+	}
+	
 }
